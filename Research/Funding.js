6c94e3a73120eb3b1ace31d80d79470491a2090d
Abhishek Niroula <abhishekniroula7@gmail.com>
2026-01-22 12:53:56 +0100

    Update2026.1

--- Research/Funding.js
+++ Research/Funding.js
@@ -4,7 +4,7 @@ document.write(`
 										
 	<p>
 	
-	<a href="https://kaw.wallenberg.org/en">Knut and Alice Wallenberg Foundation</a>
+	<a href="https://kaw.wallenberg.org/en">Knut and Alice Wallenberg Foundation</a><br>
 	<a href="https://www.scilifelab.se/data-driven/">SciLifeLab & Wallenberg Data Driven Life Science Program</a><br>
 	<a href="https://www.vr.se/english.html">Vetenskapsrådet</a><br>
 	<a href="https://www.cancerfonden.se">The Swedish Cancer Society (Cancerfonden)</a><br>
